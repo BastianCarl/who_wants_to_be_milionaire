13daf685aeea2f040c0380e8a708fa1608f4fd4d
Eusebiu Morar <morar25032002@yahoo.ro>
2025-06-14 11:29:24 +0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className='app-header'>
+      <img src='logo512.png' alt='React logo' />
+      <h1>Who Wants to Be a Millionaire?</h1>
+    </header>
+  );
+}
+
+export default Header;
